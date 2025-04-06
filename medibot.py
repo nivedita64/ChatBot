c2bd0c6669c33e66ebfcfa7a88aa728c1c6fe1bd
Nivedita <nive.shru24@gmail.com>
2025-04-06 23:47:07 +0530

    updated

--- medibot.py
+++ medibot.py
@@ -2,7 +2,7 @@ import json
 import os
 import tkinter as tk
 
-# Load responses from JSON file
+# Loading responses from JSON file
 file_path = os.path.join(os.path.dirname(__file__), "responses.json")
 with open(file_path, "r") as file:
     responses = json.load(file)
@@ -23,12 +23,12 @@ def send_message():
     chat_log.insert(tk.END, f"MediBot: {response}\n\n")
     entry.delete(0, tk.END)
 
-# Create window
+# Creating window
 root = tk.Tk()
 root.title("MediBot - Chat with me!")
 root.geometry("400x500")
 
-chat_log = tk.Text(root, bg="white", fg="black")
+chat_log = tk.Text(root, bg="light green", fg="black")
 chat_log.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
 entry = tk.Entry(root, width=50)
@@ -37,6 +37,6 @@ entry.pack(padx=10, pady=(0,10), side=tk.LEFT, expand=True, fill=tk.X)
 send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(padx=10, pady=(0,10), side=tk.RIGHT)
 
-root.mainloop()
+root.mainloop() 
 
 
